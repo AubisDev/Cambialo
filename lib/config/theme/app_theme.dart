e4f31a0f1aaf145eef107286409854e148288698
Aubis Sanchez <aubisst@gmail.com>
2025-02-24 13:14:53 -0400

    color seed for app theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
+        colorSchemeSeed: const Color.fromRGBO(245, 189, 117, 1.0),
       );
 }
