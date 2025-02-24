daf0c93789fc5603da8b68421b31d3646ef6dfb3
Aubis Sanchez <aubisst@gmail.com>
2025-02-24 11:56:57 -0400

    added app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:truequealo/config/router/app_route.dart';
 
+import 'config/theme/app_theme.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: AppTheme().getTheme(),
       routerConfig: appRoute,
-      title: 'Material App',
+      title: 'Truequealo',
     );
   }
 }
