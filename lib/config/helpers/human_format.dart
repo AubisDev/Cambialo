eedd54e827f09cfd5d0e0a73b83a23224325be4c
Aubis Sanchez <aubisst@gmail.com>
2025-02-26 18:03:46 -0400

    helper to convert dates to readable dates

---
+++ lib/config/helpers/human_format.dart
@@ -0,0 +1,14 @@
+class HumanFormat {
+  static String formatDate(DateTime date) {
+    final now = DateTime.now();
+    final difference = now.difference(date).inDays;
+
+    if (difference == 0) {
+      return 'Hoy';
+    } else if (difference == 1) {
+      return 'Ayer';
+    } else {
+      return 'Hace $difference días';
+    }
+  }
+}
