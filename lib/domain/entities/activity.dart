dfafd16c6de8d0bedeb316fc05284de856f54a0d
Aubis Sanchez <aubisst@gmail.com>
2025-02-27 16:34:22 -0400

    added activity entity

---
+++ lib/domain/entities/activity.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Activity {
+  final String id;
+  final String name;
+  final IconData icon;
+
+  Activity({required this.id, required this.name, required this.icon});
+}
