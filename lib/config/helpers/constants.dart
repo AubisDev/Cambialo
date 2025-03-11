65a4349e029fff254f0c0d18b348f09cd3f8a2c8
Aubis Sanchez <aubisst@gmail.com>
2025-03-10 23:59:53 -0400

    updated categories id type

--- lib/config/helpers/constants.dart
+++ lib/config/helpers/constants.dart
@@ -4,19 +4,18 @@ import 'package:truequealo/domain/entities/category.dart';
 import '../../domain/entities/activity.dart';
 
 final List<Category> categories = [
-  Category(
-      id: '1', name: 'Accesorios de Vehiculos', icon: Icons.directions_car),
-  Category(id: '2', name: 'Vehiculos', icon: Icons.car_rental),
-  Category(id: '3', name: 'Celulares', icon: Icons.smartphone),
-  Category(id: '4', name: 'Electronica Audio y Video', icon: Icons.headset),
-  Category(id: '5', name: 'Hogar', icon: Icons.home),
-  Category(id: '6', name: 'Construccion', icon: Icons.construction),
-  Category(id: '7', name: 'Videojuegos', icon: Icons.videogame_asset),
-  Category(id: '8', name: 'Gimnasio', icon: Icons.fitness_center),
-  Category(id: '9', name: 'Herramientas', icon: Icons.build),
-  Category(id: '10', name: 'Audio y Musica', icon: Icons.music_note),
-  Category(id: '11', name: 'Ropa', icon: Icons.checkroom),
-  Category(id: '12', name: 'Otros', icon: Icons.drag_handle),
+  Category(id: 1, name: 'Accesorios de Vehiculos', icon: Icons.directions_car),
+  Category(id: 2, name: 'Vehiculos', icon: Icons.car_rental),
+  Category(id: 3, name: 'Celulares', icon: Icons.smartphone),
+  Category(id: 4, name: 'Electronica Audio y Video', icon: Icons.headset),
+  Category(id: 5, name: 'Hogar', icon: Icons.home),
+  Category(id: 6, name: 'Construccion', icon: Icons.construction),
+  Category(id: 7, name: 'Videojuegos', icon: Icons.videogame_asset),
+  Category(id: 8, name: 'Gimnasio', icon: Icons.fitness_center),
+  Category(id: 9, name: 'Herramientas', icon: Icons.build),
+  Category(id: 10, name: 'Audio y Musica', icon: Icons.music_note),
+  Category(id: 11, name: 'Ropa', icon: Icons.checkroom),
+  Category(id: 12, name: 'Otros', icon: Icons.drag_handle),
 ];
 
 final List<Activity> activities = [
